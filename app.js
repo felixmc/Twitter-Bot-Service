dc4b740edd1b54fa79d2cab221dfa236d0998a68
Felix Milea-Ciobanu <felixmilea@gmail.com>
2014-07-28 02:36:12 -0600

    started on rest service

--- app.js
+++ app.js
@@ -1,17 +1,16 @@
 var config = require("./config.json");
 var log = require("custom-logger").config({ level: 0 });
 
-var repo = require("./repo").init(config, log);
 
-log.info("app started");
+var express 	 = require("express");
+var app     	 = express();
+var bodyParser = require("body-parser");
+app.use(bodyParser());
 
-var manyToMany = function(prop, vals) {
-	var obj = {};
-  obj[prop] = { $elemMatch: { $in: vals } };
-	return obj;
-};
+var repo   = require("./repo").init(config, log);
+var router = require("./router").init(express, repo, log);
+app.use("/api", router);
 
-repo.getStatus(repo.arrayIntersectQuery("tags", ["at"]), function(err, result) {
-	if (err) { log.error(err);  }
-	else { log.info("found: " + JSON.stringify(result)); }
-});
+app.listen(config.service_port);
+log.info("TwitterBot Service started on port " + config.service_port);
+
