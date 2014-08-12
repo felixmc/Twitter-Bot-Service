f3edf27a888d42f1114a11a4acd15e9098991eb8
Felix Milea-Ciobanu <felixmilea@gmail.com>
2014-08-12 17:22:35 -0600

    small refactoring

--- app.js
+++ app.js
@@ -1,16 +1,18 @@
-var config     = require("./config.json");
-var log        = require("custom-logger").config({ level: 0 });
-var express    = require("express");
-var app        = express();
-var mongoRest  = require("mango-rest").init(config);
+var config		= require("./config.json");
+var models		= require("./models.json");
+
+var log				= require("custom-logger").config({ level: 0, format: "%timestamp% %padding%[%event%]%message%" });
+var express		= require("express");
+var app				= express();
+var mongoRest	= require("mango-rest").init(config.mongo);
 
 // log all requests
 app.use(function(req, res, next) {
-	log.info("request to " + req.path + " from " + req.ip);
+	log.debug("request to " + req.path + " from " + req.ip);
 	next();
 });
 
-app.use("/api", mongoRest(config.models));
+app.use("/api", mongoRest(models));
 
-app.listen(config.service_port);
-log.info("TwitterBot Service started on port " + config.service_port);
+app.listen(config.service.port);
+log.info("TwitterBot Service started on port " + config.service.port);
